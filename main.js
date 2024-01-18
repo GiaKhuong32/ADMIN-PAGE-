f3fd0810aa94f98e4d829a1bcf68c95402956d06
GiaKhuong32 <khuonghoanggia3202@gmail.com>
2024-01-18 15:40:16 +0700

    web

---
+++ main.js
@@ -0,0 +1,96 @@
+// let n = 1;
+// let res = 1;
+// if(n<=10){
+//     res = n*5000
+//     console.log(res)
+// }
+   
+    
+
+//     else if(n>10 && n <= 15)
+//     {
+//         res= n*10000
+//         console.log (res);
+//     }
+//     else if(n>15 && n <= 30)
+//     {
+//         res= n*20000
+//         console.log (res);
+//     }
+//     else if(n>30 && n <= 50)
+//     {
+//         res= n*50000
+//         console.log (res);
+//     }
+//     else if(n> 50 )
+//     {
+//         res= n*60000
+//         console.log (res);
+//     }
+// let random = Math.floor(Math.random()*100);
+// let guess=prompt('nhap n');
+
+// let countGuess =1;
+// let countlimit =10;
+
+// while( random != guess && countGuess < countlimit) {
+//     if(guess > random) {
+//         guess = prompt('Nhap so be hon');
+//     } else {
+//         guess = prompt('Nhap so lon hon');
+//     }
+//     countGuess++;
+// }
+// if(guess == random) { 
+//     alert('Ban da thang!');
+// } else {
+//     alert("Ban thua r");
+// } 
+
+// let student =[ 
+//     {name:"A",
+//     heigh:"173cm",
+//     gpa:1,
+// },
+// {name:"B",
+// heigh:"175cm", 
+// gpa:9,
+// },
+// {name:"C",
+// heigh:"177cm",
+// gpa:3,
+// },
+// {name:"D",
+// heigh:"180cm", 
+// gpa:8,
+// },
+// ];
+
+// console.log(student.sort((b,a) => a.gpa - b.gpa))
+// console.log(student.sort((b,a)=> b.gpa - a.gpa))
+
+//bai moi (nhap so lan can 4 lon nhat)
+let n= 5;
+let arr = []; 
+for(let i=0;i<n;i++ ){
+    arr[i]=[];
+    for(let j=0; j<n;j++){
+        arr[i][j]=Math.floor(Math.random()* (27-13)+13);
+    }
+}
+
+console.log(arr);
+
+for( let i=0; i<n;i++){
+    for( let j=0; j<n;j++)
+}
+
+
+
+
+
+   
+
+
+
+
